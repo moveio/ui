7c25c9d4409cc3c3e9b5f738cf14d3d829c73835
mintenker <samog333@gmail.com>
2017-04-08 07:41:41 +0200

    feat(hooks): hooks getting from API

--- src/app/platform/pipeline-detail/pipeline-detail.component.ts
+++ src/app/platform/pipeline-detail/pipeline-detail.component.ts
@@ -6,8 +6,10 @@ import * as d3 from 'd3';
 import { Gesture } from '../../models/gesture.model';
 import { Store } from '@ngrx/store';
 import { LOAD_GESTURES } from '../../reducers/gesture.reducer';
+import { LOAD_HOOKS } from 'app/reducers/hooks.reducer';
+import { Hook } from '../../models/hook.model';
 
-const ADD_GESTURE = 'ADD_GESTURE';
+const ADD_ELEMENT = 'ADD_ELEMENT';
 const REMOVE = 'REMOVE';
 const NONE = 'NONE';
 const DRAW_LINE = 'DRAW_LINE';
@@ -23,21 +25,24 @@ export class PipelineDetailComponent implements OnDestroy, AfterViewInit {
   pipelineID: string;
 
   gestures: Gesture[];
+  hooks: Hook[];
   pipelines = MOCK_PIPELINES;
   selectedPipeline: any;
 
-  mode: 'ADD_GESTURE' | 'DRAW_LINE' | 'REMOVE' | 'NONE' = 'NONE';
-  newGesture: Gesture;
+  mode: 'ADD_ELEMENT' | 'DRAW_LINE' | 'REMOVE' | 'NONE' = 'NONE';
+  newElement: Gesture | Hook;
+  newElementType: string;
 
   lineGestureRect: any;
 
-  gestureOrder = 0;
+  elementOrder = 0;
   svg: any;
   mouse: any;
 
   constructor(private activeRoute: ActivatedRoute, store: Store<any>) {
 
     store.dispatch({ type: LOAD_GESTURES });
+    store.dispatch({ type: LOAD_HOOKS });
 
     this.subscriptions.push(activeRoute.params.subscribe((params) => {
       this.pipelineID = params['id'];
@@ -47,17 +52,21 @@ export class PipelineDetailComponent implements OnDestroy, AfterViewInit {
     this.subscriptions.push(store.select('gestures').subscribe((data: any) => {
       this.gestures = data;
     }));
+
+    this.subscriptions.push(store.select('hooks').subscribe((data: any) => {
+      this.hooks = data;
+    }));
   }
 
   ngAfterViewInit(): void {
     this.svg = d3.select('svg');
 
     this.svg.on('mousedown', () => {
-      if (this.mode === ADD_GESTURE) {
+      if (this.mode === ADD_ELEMENT) {
         this.mouse = d3.mouse(d3.event.currentTarget);
-        this.addGesture(this.newGesture);
+        this.addElement(this.newElement);
         this.mode = NONE;
-        this.newGesture = null;
+        this.newElement = null;
 
       }
     });
@@ -69,90 +78,73 @@ export class PipelineDetailComponent implements OnDestroy, AfterViewInit {
     }
   }
 
-  startAdding(gesture: Gesture): void {
-    this.newGesture = gesture;
-    this.mode = ADD_GESTURE;
+  startAdding(element: Gesture | Hook, type: 'gesture' | 'hook' = 'gesture'): void {
+    this.newElementType = type;
+    this.newElement = element;
+    this.mode = ADD_ELEMENT;
   }
 
   toggleRemoving(): void {
     this.mode = this.mode === REMOVE ? NONE : REMOVE;
   }
 
+  mouseDownHandler(rect: any, elementID: string, textID: string) {
+    switch (this.mode) {
+      case REMOVE :
+        console.log(elementID, textID);
+        d3.select('#' + elementID).remove();
+        d3.select('#' + textID).remove();
+        d3.selectAll('.' + elementID).remove();
+        break;
 
-  addGesture(gesture: Gesture): void {
+      case DRAW_LINE:
 
-    const gestureID = 'gesture_' + this.gestureOrder.toString();
-    const textID = 'text_' + this.gestureOrder.toString();
-    this.gestureOrder += 1;
+        this.svg.append('line')
+          .attr('stroke-width', 4)
+          .attr('x1', this.lineGestureRect.attr('x'))
+          .attr('y1', this.lineGestureRect.attr('y'))
+          .attr('x2', rect.attr('x'))
+          .attr('y2', rect.attr('y'))
+          .attr('stroke', 'black')
+          .attr('class', this.lineGestureRect.attr('id') + ' ' + elementID);
+        this.mode = NONE;
+        break;
+
+      default:
+        this.mode = DRAW_LINE;
+        this.lineGestureRect = rect;
+        break;
+    }
+  }
+
+  addElement(element: Gesture | Hook): void {
+    const color = this.newElementType === 'gesture' ? '#6699cc' : '#33bb25';
+    const elementID = 'element_' + this.elementOrder.toString();
+    const textID = 'text_' + this.elementOrder.toString();
+    this.elementOrder += 1;
 
     const rect = this.svg.append('rect')
       .attr('x', this.mouse[0])
       .attr('y', this.mouse[1])
-      .attr('width', 100)
+      .attr('width', 150)
       .attr('height', 50)
-      .attr('fill', '#6699cc')
-      .attr('id', gestureID)
-      .style('cursor', 'pointer')
-      .on('mousedown', () => {
-        switch (this.mode) {
-          case REMOVE :
-            d3.select('#' + gestureID).remove();
-            d3.select('#' + textID).remove();
-            d3.selectAll('.' + gestureID).remove();
-            break;
+      .attr('fill', color)
+      .attr('id', elementID)
+      .style('cursor', 'pointer');
 
-          case DRAW_LINE:
-
-            this.svg.append('line')
-              .attr('stroke-width', 4)
-              .attr('x1', this.lineGestureRect.attr('x'))
-              .attr('y1', this.lineGestureRect.attr('y'))
-              .attr('x2', rect.attr('x'))
-              .attr('y2', rect.attr('y'))
-              .attr('stroke', 'black')
-              .attr('class', this.lineGestureRect.attr('id') + ' ' + gestureID);
-            this.mode = NONE;
-            break;
-
-          default:
-            this.mode = DRAW_LINE;
-            this.lineGestureRect = rect;
-            break;
-        }
-      });
+    rect.on('mousedown', () => {
+      this.mouseDownHandler(rect, elementID, textID);
+    });
 
     this.svg.append('text')
-      .text(gesture.name)
+      .text(element.name)
       .attr('fill', 'white')
       .attr('x', (+this.mouse[0] + 10))
       .attr('y', (+this.mouse[1] + 30))
       .attr('id', textID)
       .style('cursor', 'pointer')
       .on('mousedown', () => {
-        switch (this.mode) {
-          case REMOVE :
-            d3.select('#' + gestureID).remove();
-            d3.select('#' + textID).remove();
-            d3.selectAll('.' + gestureID).remove();
-            break;
-
-          case DRAW_LINE:
-            this.svg.append('line')
-              .attr('stroke-width', 4)
-              .attr('x1', this.lineGestureRect.attr('x'))
-              .attr('y1', this.lineGestureRect.attr('y'))
-              .attr('x2', rect.attr('x'))
-              .attr('y2', rect.attr('y'))
-              .attr('stroke', 'black')
-              .attr('class', this.lineGestureRect.attr('id') + ' ' + gestureID);
-            this.mode = NONE;
-            break;
-
-          default:
-            this.mode = DRAW_LINE;
-            this.lineGestureRect = rect;
-            break;
-        }
+        this.mouseDownHandler(rect, elementID, textID);
       });
   }
 
